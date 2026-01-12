9bbf5ab035d83428ab88b212495fe9d7a075a0a6
Harsha S <officialharsha31@gmail.com>
2026-01-12 15:31:20 +0530

    1st Face of Project

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Course
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import CourseForm
+
 
 @login_required
 def course_list(request):
@@ -17,3 +21,20 @@ def course_detail(request, course_id):
         'lessons': lessons,
         'progress': progress
     })
+
+@login_required
+def create_course(request):
+    if request.user.role != 'ADMIN':
+        return redirect('dashboard')
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            course = form.save(commit=False)
+            course.created_by = request.user
+            course.save()
+            return redirect('courses')
+    else:
+        form = CourseForm()
+
+    return render(request, 'create_course.html', {'form': form})
