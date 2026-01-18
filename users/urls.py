c29a678364ce124fe98d62b0c2f934402356f039
TS Tamarai Selvan <69187725+tamaraiselvan@users.noreply.github.com>
2026-01-18 13:43:03 +0530

    Revert "Changes for Password reset and user Approval."
    This reverts commit 4d09bb3fa5ddb1bf3b1db9d444f9e451de076727.

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('', register, name='register'),
     path('verify-email/', verify_email, name='verify_email'),
     path('user-approval-flow/<int:user_id>/', user_approval_flow, name='user_approval_flow'),
-    path('user-approvals/', pending_user_approvals, name='pending_user_approvals'),
-    path('user-approvals/<int:user_id>/approve/', approve_user, name='approve_user')
 ]
