6e54651447f086d1cb37f75f2c3e55b12b80886d
TS Tamarai Selvan <69187725+tamaraiselvan@users.noreply.github.com>
2026-03-27 20:32:43 +0530

    email bug fix

--- brainboost_core/settings.py
+++ brainboost_core/settings.py
@@ -120,9 +120,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = '2k26brainboostofficial@gmail.com'
-EMAIL_HOST_PASSWORD = 'zywwfeoageisolsi'
+EMAIL_HOST_PASSWORD = 'wzpospvfgcgzehcf'
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
